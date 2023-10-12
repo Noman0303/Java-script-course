eae29e844cac476ae0e3ca8f12dc7b2ae31a96d4
Noman0303 <nomanbutex38@gmail.com>
2023-10-13 00:29:12 +0600

    first commit

---
+++ 4-comments.js
@@ -0,0 +1,15 @@
+// Single Line Comments
+// Single line comments start with //.
+
+// Multi-line Comments
+// Multi-line comments start with /* and end with */.
+
+
+// hello js // .......
+
+
+/*
+HTML
+CSS
+JS
+*/
---
+++ 6-operators.js
@@ -0,0 +1,37 @@
+// Assignment Operator (=) assigns a value to a variable
+
+// Assign the value 5 to x
+let x = 5;
+// Assign the value 2 to y
+let y = 2;
+
+
+// Assign the value x + y to z:
+let z = x + y;
+
+// Addition Operator (+) adds numbers:
+let sum = x + y;
+let a = x -y;
+
+
+// console.log(sum);
+// console.log(a);
+
+//  Multiplication Operator (*) multiplies numbers:
+// z = x / y;
+// z = x % y;
+
+z = x ** 3;
+
+console.log(z);
+
+
+
+
+
+
+
+/**
+    => Watch This Video
+    https://youtu.be/UzPsbiJjqsA?si=vS71Qtogu4lYqtl5 
+ */
